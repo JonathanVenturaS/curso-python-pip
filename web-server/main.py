7e547f13948d46e55cee511b1ef9c0142db0ea67
JonathanVenturaS <johnnyventura1610@gmail.com>
2023-01-26 13:52:56 -0600

    ultimas actualizaciones

---
+++ web-server/main.py
@@ -0,0 +1,18 @@
+import store
+from fastapi import FastApi
+
+app = FastApi()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact')
+def get_list():
+    return {'name': 'Platzi'}
+
+def run():
+    store.get_categories()
+
+if __name__ == '__main__':
+    run()
